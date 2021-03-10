9da1e07d7584da5a37639b8253986a466f9bd84e
MERCIERClement <clementmercierprevert@gmail.com>
2021-03-10 10:40:06 +0100

    Exo 3 Q 3

---
+++ TP2/Exo 3/template_header.php
@@ -0,0 +1,8 @@
+<!DOCTYPE html>
+<html>
+    <head>
+    <title>Clément Mercier</title>
+        <link rel="stylesheet" href="bootstrap.css" type="text/css"
+            media="screen" title="default" charset="UTF-8" />
+    </head>
+    <body>
