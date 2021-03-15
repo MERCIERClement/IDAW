0aa342e8e985bd3c490116fe29dbe79f67d811e5
MERCIERClement <clementmercierprevert@gmail.com>
2021-03-15 14:29:48 +0100

    1.1

--- TP3/connected.php
+++ TP3/connected.php
@@ -1,6 +1,25 @@
 <?php
-$login = $_GET['login'];
-$password = $_GET['password'];
-echo $password;
-echo $login;
+    $users = array(
+        'riri' => 'fifi',
+        'yoda' => 'maitrejedi');
+    $login="anonymours";
+    $errorText = "";
+    $successfullyLogged = false;
+
+    if(isset($_GET['login']) && isset($_GET['password'])) {
+        $tryLogin=$_GET['login'];    
+        $tryPwd=$_GET['password'];
+        if(array_key_exists($tryLogin,$users) && $users[$tryLogin]==$tryPwd) {
+            $successfullyLogged = true;
+            $login = $tryLogin;
+        } else {
+            $errorText = "Erreur de login/password";
+    }} else {
+        $errorText = "Merci d'utiliser le formulaire de login";
+    }
+    if(!$successfullyLogged) {
+        echo $errorText;
+    } else {
+        echo "<h1>Bienvenue ".$login."</h1>";
+    }
 ?>
